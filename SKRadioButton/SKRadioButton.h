efccc7d39421eb2cad66fd2f444208c00dbb85f4
Sagar Kothari <Sagar.Kothari@MasterCard.com>
2017-11-09 17:34:28 +0530

    Radio button first version

---
+++ SKRadioButton/SKRadioButton.h
@@ -0,0 +1,19 @@
+//
+//  SKRadioButton.h
+//  SKRadioButton
+//
+//  Created by Kothari, Sagar on 11/9/17.
+//  Copyright © 2017 Kothari, Sagar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKRadioButton.
+FOUNDATION_EXPORT double SKRadioButtonVersionNumber;
+
+//! Project version string for SKRadioButton.
+FOUNDATION_EXPORT const unsigned char SKRadioButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKRadioButton/PublicHeader.h>
+
+
